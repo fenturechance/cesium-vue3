0ab69ebfac6edd284cb2b7816a7b2cebb8343e24
jerryho <jerryho2@fareastone.com.tw>
2025-01-06 11:40:22 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+import "cesium/Build/Cesium/Widgets/widgets.css";
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from "vite-plugin-static-copy";
+const cesiumSource = "node_modules/cesium/Build/Cesium";
+const cesiumBaseUrl = "cesiumStatic";
+
+// https://vite.dev/config/
+export default defineConfig({
+  define: {
+    // Define relative base path in cesium for loading assets
+    // https://vitejs.dev/config/shared-options.html#define
+    CESIUM_BASE_URL: JSON.stringify(`/${cesiumBaseUrl}`),
+  },
+  plugins: [
+    vue(),
+    vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
+        { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
+        { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
+        { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
